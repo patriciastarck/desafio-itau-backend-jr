e47965d20b2dfe04a5f1b1e3664cc1d634d694c9
Patricia <patriciastarck@gmail.com>
2025-03-10 15:46:34 -0300

    Criação do StatisticsController

---
+++ src/main/java/io/github/patriciastarck/desafio_itau_backend_jr/controller/StatisticsController.java
@@ -0,0 +1,29 @@
+package io.github.patriciastarck.desafio_itau_backend_jr.controller;
+
+import io.github.patriciastarck.desafio_itau_backend_jr.dto.StatisticsResponse;
+import io.github.patriciastarck.desafio_itau_backend_jr.service.TransactionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.DoubleSummaryStatistics;
+
+@RestController
+@RequestMapping("/estatistica")
+public class StatisticsController {
+
+    private final TransactionService transactionService;
+
+    public StatisticsController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @GetMapping
+    public ResponseEntity<StatisticsResponse>getStatistics() {
+        DoubleSummaryStatistics stats = transactionService.getStatistics();
+        return ResponseEntity.ok(new StatisticsResponse(stats));
+
+    }
+}
