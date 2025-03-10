423cc22044f17158da52deade107ed486429c50f
Patricia <patriciastarck@gmail.com>
2025-03-10 11:49:12 -0300

    Iniciando projeto

---
+++ src/main/java/io/github/patriciastarck/desafio_itau_backend_jr/DesafioItauBackendJrApplication.java
@@ -0,0 +1,13 @@
+package io.github.patriciastarck.desafio_itau_backend_jr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioItauBackendJrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioItauBackendJrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/patriciastarck/desafio_itau_backend_jr/DesafioItauBackendJrApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.patriciastarck.desafio_itau_backend_jr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioItauBackendJrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
