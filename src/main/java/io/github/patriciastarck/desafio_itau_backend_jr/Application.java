e4ea3d1cfcc580b2cd5771982d2e477b63e3faf5
Patricia <patriciastarck@gmail.com>
2025-03-10 15:41:07 -0300

    Configuração da Application.

--- src/main/java/io/github/patriciastarck/desafio_itau_backend_jr/Application.java
+++ src/main/java/io/github/patriciastarck/desafio_itau_backend_jr/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DesafioItauBackendJrApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DesafioItauBackendJrApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
